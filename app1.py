9ae6114e586e06c9bc0756c43021ff163c23f004
byelllopes <75124840+byelllopes@users.noreply.github.com>
2026-05-20 18:39:04 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -93,4 +93,4 @@ if st.button("Calcular e Gerar Relatório", type="primary"):
         st.write(f"Reação em Y (RAy): `{R_Ay:.2f} lb`")
     with col_C:
         st.write("**Mancal C (Apoio, z = 20 pol):**")
-        st.write(f"Reação em X (RCx): `{R_Cx:.2f}
+     st.write(f"Reação em Y (RCy): `{R_Cy:.2f} lb`")
